60793e9974c09e18e3f699541ae5040015ed88e1
Rahma Maulida <rahma.maulida68@gmail.com>
2022-08-09 19:37:07 +0700

    Edit

---
+++ pages/admin/login.js
@@ -0,0 +1,5 @@
+export default function AdminLogin() {
+    return (
+        <div></div>
+    )
+}
---
+++ pages/super-admin/admin-register.js
@@ -0,0 +1,5 @@
+export default function AdminRegister() {
+    return (
+        <div></div>
+    )
+}
